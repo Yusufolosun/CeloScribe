36dca2a4383768b946407122b64512096f6414e8
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-04-06 03:13:12 +0100

    feat(contracts): add SPDX license, pragma, and OpenZeppelin imports

---
+++ packages/contracts/contracts/CeloScribePayment.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
