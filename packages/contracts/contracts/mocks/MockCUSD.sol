f0084c9527552c03ff73a39700dec691884e7b79
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-04-06 03:13:22 +0100

    test(contracts): add public mint function to MockCUSD

--- packages/contracts/contracts/mocks/MockCUSD.sol
+++ packages/contracts/contracts/mocks/MockCUSD.sol
@@ -5,4 +5,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockCUSD is ERC20 {
     constructor() ERC20("Mock cUSD", "mcUSD") {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
 }
